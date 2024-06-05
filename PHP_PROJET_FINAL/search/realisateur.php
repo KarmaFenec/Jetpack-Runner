3030ec16184cf40328b61df4c5d990209524a644
ProjectJ62 <noe.duperrin@gmail.com>
2024-06-05 09:06:59 +0200

    Update realisateur.php

--- PHP_PROJET_FINAL/search/realisateur.php
+++ PHP_PROJET_FINAL/search/realisateur.php
@@ -11,7 +11,7 @@ $logged = isset($_SESSION['nickname']) ;
 
 //On récupère la base de donnée
 $table = "personne";
-$sql = "SELECT * FROM film ORDER BY nom WHERE metier = real";
+$sql = "SELECT * FROM film ORDER BY nom WHERE metier = 'réalisateur'";
 $fdb = new FilmPDO() ;
 $fhtml = new FilmHTML();
 $data =$fdb->genererRequest($sql);

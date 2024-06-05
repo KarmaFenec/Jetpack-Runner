2a421b58579675cbada0004add19a5b31ddb5c6c
ProjectJ62 <noe.duperrin@gmail.com>
2024-06-05 09:25:56 +0200

    Update film.php

--- PHP_PROJET_FINAL/search/film.php
+++ PHP_PROJET_FINAL/search/film.php
@@ -36,12 +36,6 @@ $data =$fdb->genererRequest($sql);
     </div>
 </nav>
 
-<?php //A FAIRE ?>
-<?php if ($logged):?>
-    <div class="dropdown">
-        <a class="btn btn-dark" role="button" href="../client/acteur.php">EDIT</a>
-    </div>
-<?php endif; ?>
 </header>
 
 <div class="main-content">

97cf02d0af3392ec345e287ca98d6e31a3101e17
ProjectJ62 <noe.duperrin@gmail.com>
2024-06-07 12:13:04 +0200

    Update FilmDB.php

--- projet/class/pdb/FilmDB.php
+++ projet/class/pdb/FilmDB.php
@@ -5,13 +5,14 @@ use \pdo\FilmPDO;
 class FilmDB
 {
     protected $fdb;
+    protected $nbre_films;
 
     public function createFilm($name, $imgFile = null, $description = null, $genre = null, $reals = null, $acts = null, $anSortie = null, $duree = null)
     {
-        // On doit pouvoir créer un film/acteur/réalisateur
+        // On doit pouvoir créer un film
         $fdb = new FilmPDO();
-        $bd = $fdb->genererRequest('SELECT COUNT(*) AS nbFilm FROM film');
-        $id = $bd + 1;
+        $id = $fdb->genererRequest('SELECT COUNT(*) AS nbFilm FROM film ;');
+        $id = $id + 1;
         $name = htmlspecialchars($name);
         $description = htmlspecialchars($description);
         $genre = htmlspecialchars($genre);
@@ -35,9 +36,29 @@ class FilmDB
             echo "NO IMAGE !!!!";
         }
 
-        $query = "INSERT INTO film VALUES ('$id', '$name', '$dirImg', '$description', '$genre', '$reals', '$acts', '$anSortie', '$view' ,'$duree')";
+        $query = "INSERT INTO film VALUES ('$id', '$name', '$dirImg', '$description', '$genre', '$reals', '$acts', '$anSortie', '$view' ,'$duree');";
         $data = $fdb->genererRequest($query);
         header("Location: pages/accueil.php"); // vers accueil.php
         return $query;
     }
+
+    public function deleteFilm($name)
+    {
+        // On doit pouvoir delete un film
+        $id = $fdb->genererRequest("SELECT id FROM film WHERE name LIKE ".$name.";");
+        $query = "DELETE FROM film WHERE (".$id.");";
+        $data = $fdb->genererRequest($query);
+        header("Location: pages/accueil.php"); // vers accueil.php
+        return $query;
+    }
+
+    public function updateFilm($id, $column, $value)
+    {
+        // On doit pouvoir delete un film
+        $query = "UPDATE film SET ".$column." = ".$value." WHERE ('$id')";
+        $data = $fdb->genererRequest($query);
+        header("Location: pages/accueil.php"); // vers accueil.php
+        return $query;
+    }
+
 }

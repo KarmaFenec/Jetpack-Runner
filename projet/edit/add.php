d09c5b22940e5d86ee4ccd7e00e157a266aeb544
ProjectJ62 <noe.duperrin@gmail.com>
2024-06-07 09:42:28 +0200

    Update add.php

--- projet/edit/add.php
+++ projet/edit/add.php
@@ -4,8 +4,9 @@ Autoloader::register();
 session_start() ;
 $logged = isset($_SESSION['nickname']) ;
 ?>
-<link rel = "stylesheet" href = "../css/add.css">
 
+<link rel = "stylesheet" href = "../css/add.css">
+<?php ob_start() ; ?>
 <body>
 <div id = "main_content" style = "flex-direction: column; justify-content: center; align-items: center;">
     <h2 id = "titre">Voulez-vous ajouter:</h2>
@@ -21,3 +22,8 @@ $logged = isset($_SESSION['nickname']) ;
 </body>
 
 <script src = "../javascript/add.js"></script>
+
+<!-- Récupère le contenu du buffer (et le vide) -->
+<?php $content=ob_get_clean() ?>
+<!-- Utilisation du contenu bufferisé -->
+<?php Template::render($content) ?>

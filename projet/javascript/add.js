6755b72105f9744f4e35d297ff93596488f164b1
ProjectJ62 <noe.duperrin@gmail.com>
2024-06-07 10:01:18 +0200

    Update add.js

--- projet/javascript/add.js
+++ projet/javascript/add.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded" , function (){
     let container = document.getElementsByClassName("before");
-    console.log(container);
     for(let i = 0; i < container.length; i++){
-        console.log("caca");
         let element = container[i];
         element.addEventListener("mousemove", function (event){
             element.classList.remove("before");
@@ -13,7 +11,6 @@ document.addEventListener("DOMContentLoaded" , function (){
         element.addEventListener("mouseout", function (event){
             element.classList.remove("after");
             element.classList.add("before");
-            console.log("caca2");
         })
     }
 });
